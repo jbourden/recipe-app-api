203507dff8ac1d9d652d0185da9aa35c3fa2d218
jbourden <77255535+jbourden@users.noreply.github.com>
2022-09-14 21:48:23 -0230

    lint test

--- app/app/calc.py
+++ app/app/calc.py
@@ -5,16 +5,24 @@ Calculator functions
 
 def add(x,y):
     '''Add x and y and return result'''
+
+
     return x + y
 
 def subtract(x,y):
     '''Add x and y and return result'''
+
+
     return x - y
 
 def multiply(x,y):
     '''Add x and y and return result'''
+
+
     return x * y
 
 def divide(x,y):
     '''Add x and y and return result'''
+
+    
     return x // y
